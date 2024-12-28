103aa65af99b893732c7c88ed3b985e926b487cb
faresamireldin <git@stackblitz.com>
2024-12-28 04:10:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sb1-xhzygkba
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/faresamireldin/sb1-xhzygkba)
---
+++ src/data/characters.ts
@@ -0,0 +1,46 @@
+import { Character } from '../types/story';
+
+export const characters: Character[] = [
+  {
+    id: "natalie",
+    name: "Natalie",
+    avatar: "/avatars/natalie.jpg",
+    role: "protagonist"
+  },
+  {
+    id: "alex",
+    name: "Alex",
+    avatar: "/avatars/alex.jpg",
+    role: "student"
+  },
+  {
+    id: "sarah",
+    name: "Sarah",
+    avatar: "/avatars/sarah.jpg",
+    role: "group_admin"
+  },
+  {
+    id: "james",
+    name: "James",
+    avatar: "/avatars/james.jpg",
+    role: "student"
+  },
+  {
+    id: "maya",
+    name: "Maya",
+    avatar: "/avatars/maya.jpg",
+    role: "naya_friend"
+  },
+  {
+    id: "david",
+    name: "David",
+    avatar: "/avatars/david.jpg",
+    role: "teacher"
+  },
+  {
+    id: "naya",
+    name: "Naya",
+    avatar: "/avatars/naya.jpg",
+    role: "missing"
+  }
+];
